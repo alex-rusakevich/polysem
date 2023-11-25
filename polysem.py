56398e786db2a30cd76cfae7bf483093f9df604d
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-25 15:58:18 +0300

    #patch added colors to the program

--- polysem.py
+++ polysem.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
+import colorama
+from colorama import Back, Fore, Style
+
 from polysem.logics import *
 from polysem.settings import *
 
@@ -10,33 +13,53 @@ if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
 
+colorama.init(autoreset=True)
+
+
 def main():
     print("The program was created by Alexander Rusakevich, 402/1, Chinese faculty")
-    print(f"The core word is '{THE_WORD}'")
+    the_yellow_word = Fore.YELLOW + "'" + THE_WORD + "'" + Style.RESET_ALL
+    print(f"The core word is {the_yellow_word}")
 
     if len(sys.argv) == 1:  # REPL
         print("Press Ctrl-Z or Ctrl-C to stop")
 
         while True:
-            stem_seqs = text_to_stem_seqs(input("\nText: "))
+            stem_seqs = text_to_stem_seqs(
+                input(Fore.YELLOW + "\nText: " + Style.RESET_ALL)
+            )
 
             if stem_seqs == []:
-                print(f"No sentence with word '{THE_WORD}', skipping...")
+                print(Fore.RED + f"No sentence with word '{THE_WORD}', skipping...")
                 continue
 
             for orig_sent, sent_stems in stem_seqs:
-                print(f"\nProcessing sentence '{orig_sent}'...")
+                print(Fore.YELLOW + f"\nProcessing sentence '{orig_sent}'...")
 
                 meanings = get_seq_meanings(stems_to_meaning_seq(sent_stems))
 
                 if not meanings:
-                    print("Couldn't estimate the meaning, please, try another sentence")
+                    print(
+                        Fore.RED
+                        + "Couldn't estimate the meaning, please, try another sentence"
+                    )
+                    continue
                 else:
                     for meaning, score in meanings:
-                        print(f"\n-> Meaning with score {score}:", meaning.text)
+                        print(
+                            Fore.GREEN
+                            + "\n-> "
+                            + Style.RESET_ALL
+                            + "Meaning with score "
+                            + Fore.GREEN
+                            + str(score)
+                            + Style.RESET_ALL
+                            + ":",
+                            meaning.text,
+                        )
                         print("Example:", meaning.example)
 
-            print("\nDone.")
+            print(Fore.GREEN + "\nDone.")
 
     else:
         raise NotImplementedError("Command-line args are not supported yet")
